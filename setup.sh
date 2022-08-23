aaabd0464143f170f49e1d19202a789f1bebb3e4
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-08-23 09:48:40 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -211,38 +211,22 @@ read answer
         cp /etc/xray/domain /root/domain
     else
         clear
-        yellow "Change Domain for vmess/vless/trojan dll"
-        echo " "
-        read -rp "Input ur domain : " -e pp
-            if [ -z $pp ]; then
-                echo -e "
-                Nothing input for domain!
-                Then a random domain will be created
-                "
-                sleep 2
-                sub=scvps`</dev/urandom tr -dc a-z0-9 | head -c4`
-                echo "peler=${sub}" > /root/scdomain
-            else
-                echo "peler=$pp" > /root/scdomain
-            fi
-        #wget -q "https://raw.githubusercontent.com/myridwan/vip/ipuk/dll/cf.sh" && chmod +x cf.sh && ./cf.sh
-    fi
-else
-clear
 yellow "Add Domain for vmess/vless/trojan dll"
 echo " "
 read -rp "Input ur domain : " -e pp
     if [ -z $pp ]; then
         echo -e "
         Nothing input for domain!
-        Then a random domain will be created
-        "
-        sleep 2
-        sub=scvps`</dev/urandom tr -dc a-z0-9 | head -c4`
-        echo "peler=${sub}" > /root/scdomain
+        Then a random domain will be created"
     else
-        echo "peler=$pp" > /root/scdomain
+        echo "$pp" > /root/scdomain
+	echo "$pp" > /etc/xray/scdomain
+	echo "$pp" > /etc/xray/domain
+	echo "$pp" > /etc/v2ray/domain
+	echo $pp > /root/domain
+        echo "IP=$pp" > /var/lib/scrz-prem/ipvps.conf
     fi
+    
 wget -q "https://raw.githubusercontent.com/myridwan/vip/ipuk/dll/cf.sh" && chmod +x cf.sh && ./cf.sh
 fi
 
